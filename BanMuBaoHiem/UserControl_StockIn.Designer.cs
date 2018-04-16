7b54f0bf7551f0345862c42f788c96f5e2609448
huygiangit96 <32055253+huygiangit96@users.noreply.github.com>
2018-04-16 10:29:01 +0700

    Report

--- BanMuBaoHiem/UserControl_StockIn.Designer.cs
+++ BanMuBaoHiem/UserControl_StockIn.Designer.cs
@@ -29,13 +29,6 @@
         private void InitializeComponent()
         {
             this.dtg_stockin = new System.Windows.Forms.DataGridView();
-            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Prod_id = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Prod_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Brand = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Quantity = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Unit_price = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.date_col = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.btn_insert = new System.Windows.Forms.Button();
             this.btn_edit = new System.Windows.Forms.Button();
             this.btn_delete = new System.Windows.Forms.Button();
@@ -46,15 +39,23 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.txb_si_code = new System.Windows.Forms.TextBox();
-            this.txb_pro_brand = new System.Windows.Forms.TextBox();
             this.txb_pro_id = new System.Windows.Forms.TextBox();
             this.txb_pro_quantity = new System.Windows.Forms.TextBox();
             this.dtp_pro_date = new System.Windows.Forms.DateTimePicker();
             this.label6 = new System.Windows.Forms.Label();
-            this.txb_pro_name = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
             this.txb_pro_up = new System.Windows.Forms.TextBox();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_lammoi_stockin = new System.Windows.Forms.Button();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Prod_id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Prod_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Brand = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Quantity = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Unit_price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.date_col = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cb_pro_name = new System.Windows.Forms.ComboBox();
+            this.txb_pro_brand = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dtg_stockin)).BeginInit();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -75,48 +76,6 @@
             this.dtg_stockin.Size = new System.Drawing.Size(782, 274);
             this.dtg_stockin.TabIndex = 0;
             // 
-            // ID
-            // 
-            this.ID.DataPropertyName = "ID";
-            this.ID.HeaderText = "Mã nhập kho";
-            this.ID.Name = "ID";
-            // 
-            // Prod_id
-            // 
-            this.Prod_id.DataPropertyName = "ProdID";
-            this.Prod_id.HeaderText = "Mã sản phẩm";
-            this.Prod_id.Name = "Prod_id";
-            // 
-            // Prod_name
-            // 
-            this.Prod_name.DataPropertyName = "ProdName";
-            this.Prod_name.HeaderText = "Tên sản phẩm";
-            this.Prod_name.Name = "Prod_name";
-            // 
-            // Brand
-            // 
-            this.Brand.DataPropertyName = "BrandName";
-            this.Brand.HeaderText = "Hãng";
-            this.Brand.Name = "Brand";
-            // 
-            // Quantity
-            // 
-            this.Quantity.DataPropertyName = "Quantity";
-            this.Quantity.HeaderText = "Số lượng";
-            this.Quantity.Name = "Quantity";
-            // 
-            // Unit_price
-            // 
-            this.Unit_price.DataPropertyName = "UnitPrice";
-            this.Unit_price.HeaderText = "Đơn giá";
-            this.Unit_price.Name = "Unit_price";
-            // 
-            // date_col
-            // 
-            this.date_col.DataPropertyName = "date";
-            this.date_col.HeaderText = "Ngày nhập";
-            this.date_col.Name = "date_col";
-            // 
             // btn_insert
             // 
             this.btn_insert.Location = new System.Drawing.Point(84, 152);
@@ -128,16 +87,17 @@
             // 
             // btn_edit
             // 
-            this.btn_edit.Location = new System.Drawing.Point(282, 152);
+            this.btn_edit.Location = new System.Drawing.Point(240, 152);
             this.btn_edit.Name = "btn_edit";
             this.btn_edit.Size = new System.Drawing.Size(75, 23);
             this.btn_edit.TabIndex = 1;
             this.btn_edit.Text = "Sửa";
             this.btn_edit.UseVisualStyleBackColor = true;
+            this.btn_edit.Click += new System.EventHandler(this.btn_edit_Click);
             // 
             // btn_delete
             // 
-            this.btn_delete.Location = new System.Drawing.Point(461, 152);
+            this.btn_delete.Location = new System.Drawing.Point(410, 152);
             this.btn_delete.Name = "btn_delete";
             this.btn_delete.Size = new System.Drawing.Size(75, 23);
             this.btn_delete.TabIndex = 2;
@@ -204,20 +164,15 @@
             this.txb_si_code.Enabled = false;
             this.txb_si_code.Location = new System.Drawing.Point(84, 20);
             this.txb_si_code.Name = "txb_si_code";
+            this.txb_si_code.ReadOnly = true;
             this.txb_si_code.Size = new System.Drawing.Size(164, 20);
             this.txb_si_code.TabIndex = 9;
-            // 
-            // txb_pro_brand
-            // 
-            this.txb_pro_brand.Location = new System.Drawing.Point(363, 20);
-            this.txb_pro_brand.Name = "txb_pro_brand";
-            this.txb_pro_brand.Size = new System.Drawing.Size(164, 20);
-            this.txb_pro_brand.TabIndex = 10;
             // 
             // txb_pro_id
             // 
             this.txb_pro_id.Location = new System.Drawing.Point(84, 66);
             this.txb_pro_id.Name = "txb_pro_id";
+            this.txb_pro_id.ReadOnly = true;
             this.txb_pro_id.Size = new System.Drawing.Size(164, 20);
             this.txb_pro_id.TabIndex = 11;
             // 
@@ -246,13 +201,6 @@
             this.label6.TabIndex = 14;
             this.label6.Text = "Tên sản phẩm";
             // 
-            // txb_pro_name
-            // 
-            this.txb_pro_name.Location = new System.Drawing.Point(84, 107);
-            this.txb_pro_name.Name = "txb_pro_name";
-            this.txb_pro_name.Size = new System.Drawing.Size(164, 20);
-            this.txb_pro_name.TabIndex = 15;
-            // 
             // label7
             // 
             this.label7.AutoSize = true;
@@ -272,14 +220,15 @@
             // panel1
             // 
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.txb_pro_brand);
+            this.panel1.Controls.Add(this.cb_pro_name);
+            this.panel1.Controls.Add(this.btn_lammoi_stockin);
             this.panel1.Controls.Add(this.txb_pro_up);
             this.panel1.Controls.Add(this.label7);
-            this.panel1.Controls.Add(this.txb_pro_name);
             this.panel1.Controls.Add(this.label6);
             this.panel1.Controls.Add(this.dtp_pro_date);
             this.panel1.Controls.Add(this.txb_pro_quantity);
             this.panel1.Controls.Add(this.txb_pro_id);
-            this.panel1.Controls.Add(this.txb_pro_brand);
             this.panel1.Controls.Add(this.txb_si_code);
             this.panel1.Controls.Add(this.label5);
             this.panel1.Controls.Add(this.label4);
@@ -294,6 +243,74 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(782, 195);
             this.panel1.TabIndex = 4;
+            // 
+            // btn_lammoi_stockin
+            // 
+            this.btn_lammoi_stockin.Location = new System.Drawing.Point(541, 152);
+            this.btn_lammoi_stockin.Name = "btn_lammoi_stockin";
+            this.btn_lammoi_stockin.Size = new System.Drawing.Size(75, 23);
+            this.btn_lammoi_stockin.TabIndex = 18;
+            this.btn_lammoi_stockin.Text = "Làm mới";
+            this.btn_lammoi_stockin.UseVisualStyleBackColor = true;
+            this.btn_lammoi_stockin.Click += new System.EventHandler(this.btn_lammoi_stockin_Click);
+            // 
+            // ID
+            // 
+            this.ID.DataPropertyName = "ID";
+            this.ID.HeaderText = "Mã nhập kho";
+            this.ID.Name = "ID";
+            // 
+            // Prod_id
+            // 
+            this.Prod_id.DataPropertyName = "ProdID";
+            this.Prod_id.HeaderText = "Mã sản phẩm";
+            this.Prod_id.Name = "Prod_id";
+            // 
+            // Prod_name
+            // 
+            this.Prod_name.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Prod_name.DataPropertyName = "ProdName";
+            this.Prod_name.HeaderText = "Tên sản phẩm";
+            this.Prod_name.Name = "Prod_name";
+            // 
+            // Brand
+            // 
+            this.Brand.DataPropertyName = "BrandName";
+            this.Brand.HeaderText = "Hãng";
+            this.Brand.Name = "Brand";
+            // 
+            // Quantity
+            // 
+            this.Quantity.DataPropertyName = "Quantity";
+            this.Quantity.HeaderText = "Số lượng";
+            this.Quantity.Name = "Quantity";
+            // 
+            // Unit_price
+            // 
+            this.Unit_price.DataPropertyName = "UnitPrice";
+            this.Unit_price.HeaderText = "Đơn giá";
+            this.Unit_price.Name = "Unit_price";
+            // 
+            // date_col
+            // 
+            this.date_col.DataPropertyName = "date";
+            this.date_col.HeaderText = "Ngày nhập";
+            this.date_col.Name = "date_col";
+            // 
+            // cb_pro_name
+            // 
+            this.cb_pro_name.FormattingEnabled = true;
+            this.cb_pro_name.Location = new System.Drawing.Point(88, 106);
+            this.cb_pro_name.Name = "cb_pro_name";
+            this.cb_pro_name.Size = new System.Drawing.Size(160, 21);
+            this.cb_pro_name.TabIndex = 20;
+            // 
+            // txb_pro_brand
+            // 
+            this.txb_pro_brand.Location = new System.Drawing.Point(363, 20);
+            this.txb_pro_brand.Name = "txb_pro_brand";
+            this.txb_pro_brand.Size = new System.Drawing.Size(164, 20);
+            this.txb_pro_brand.TabIndex = 21;
             // 
             // UserControl_StockIn
             // 
@@ -323,15 +340,14 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox txb_si_code;
-        private System.Windows.Forms.TextBox txb_pro_brand;
         private System.Windows.Forms.TextBox txb_pro_id;
         private System.Windows.Forms.TextBox txb_pro_quantity;
         private System.Windows.Forms.DateTimePicker dtp_pro_date;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox txb_pro_name;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.TextBox txb_pro_up;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn_lammoi_stockin;
         private System.Windows.Forms.DataGridViewTextBoxColumn ID;
         private System.Windows.Forms.DataGridViewTextBoxColumn Prod_id;
         private System.Windows.Forms.DataGridViewTextBoxColumn Prod_name;
@@ -339,5 +355,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Quantity;
         private System.Windows.Forms.DataGridViewTextBoxColumn Unit_price;
         private System.Windows.Forms.DataGridViewTextBoxColumn date_col;
+        private System.Windows.Forms.ComboBox cb_pro_name;
+        private System.Windows.Forms.TextBox txb_pro_brand;
     }
 }
