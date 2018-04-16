1bb5cfb3060a5758659c07c77dbbfe9df04d2924
Diagru25 <31183495+Diagru25@users.noreply.github.com>
2018-04-16 10:30:40 +0700

    Revert "Revert "Merge branch 'master' of https://github.com/Diagru25/BanMuBaoHiem""
    This reverts commit 33dcebe9feea379e278b0a625f651168f94222fa.

--- BanMuBaoHiem/UserControl_StockIn.cs
+++ BanMuBaoHiem/UserControl_StockIn.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using BanMuBaoHiem.DAO;
+using BanMuBaoHiem.EF;
 
 namespace BanMuBaoHiem
 {
@@ -27,7 +28,7 @@ namespace BanMuBaoHiem
         {
             txb_si_code.DataBindings.Add("Text", dtg_stockin.DataSource, "ID", true, DataSourceUpdateMode.Never);
             txb_pro_id.DataBindings.Add("Text", dtg_stockin.DataSource, "ProdID", true, DataSourceUpdateMode.Never);
-            txb_pro_name.DataBindings.Add("Text", dtg_stockin.DataSource, "ProdName", true, DataSourceUpdateMode.Never);
+            cb_pro_name.DataBindings.Add("Text", dtg_stockin.DataSource, "ProdName", true, DataSourceUpdateMode.Never);
             txb_pro_brand.DataBindings.Add("Text", dtg_stockin.DataSource, "BrandName", true, DataSourceUpdateMode.Never);
             txb_pro_quantity.DataBindings.Add("Text", dtg_stockin.DataSource, "Quantity", true, DataSourceUpdateMode.Never);
             txb_pro_up.DataBindings.Add("Text", dtg_stockin.DataSource, "UnitPrice", true, DataSourceUpdateMode.Never);
@@ -42,12 +43,31 @@ namespace BanMuBaoHiem
         private void LoadEmp()
         {
             list.DataSource = new StockInDAO().ListAll();
+            cb_pro_name.DataSource = new ProductDAO().ListAll();
+            cb_pro_name.ValueMember = "ID";
+            cb_pro_name.DisplayMember = "Name";
         }
 
         private void btn_clear_Click(object sender, EventArgs e)
         {
             Report rp = new Report();
             rp.Show(this);
+        }
+
+        private void btn_lammoi_stockin_Click(object sender, EventArgs e)
+        {
+            txb_pro_id.Clear();
+            txb_pro_quantity.Clear();
+            txb_pro_up.Clear();
+            txb_si_code.Clear();
+        }
+
+        private void btn_edit_Click(object sender, EventArgs e)
+        {
+            //StockIn item = new StockIn();
+            //item.ProdID = Convert.ToInt64(cb_pro_name.SelectedValue);
+            //item.Quantity = Convert.ToInt64(txb_pro_quantity.Text);
+            //item
         }
     }
 }
